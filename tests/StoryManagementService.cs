2027fe9e60f0c413d93f6c484489e70770691858
noonlit <julietfeynman@gmail.com>
2021-07-02 20:40:14 +0300

    Add more tests

--- tests/StoryManagementService.cs
+++ tests/StoryManagementService.cs
@@ -50,6 +50,12 @@ namespace tests
 			{
 				_context.Remove(story);
 			}
+
+			foreach (var tag in _context.Tags)
+			{
+				_context.Remove(tag);
+			}
+
 			_context.SaveChanges();
 		}
 
@@ -59,6 +65,31 @@ namespace tests
 			var response = await _service.GetStories();
 			var count = response.ResponseOk.Entities.Count;
 			Assert.AreEqual(2, count);
+		}
+
+		[Test]
+		public async Task TestGetFilteredStories()
+		{
+			var story = await _context.Stories
+				.FirstOrDefaultAsync();
+
+			var tag = new Tag { Name = "Tag" };
+			var result = await _service.AddTagToStory(story.Id, tag);
+
+			Assert.True(result.ResponseError == null);
+
+			var stories = await _service.GetFilteredStories(tag.Id);
+			Assert.AreEqual(1, stories.ResponseOk.Entities.Count);
+		}
+
+		[Test]
+		public async Task TestGetStory()
+		{
+			var story = await _context.Stories.FirstOrDefaultAsync();
+			var storyId = story.Id;
+
+			var response = await _service.GetStory(storyId);
+			Assert.AreEqual(storyId, response.ResponseOk.Story.Id);
 		}
 
 		[Test]
@@ -122,8 +153,95 @@ namespace tests
 			_context.Entry(story).State = EntityState.Modified;
 			_context.SaveChanges();
 
-			var reloadedStory = await _context.Stories.FindAsync(story.Id);
-			Assert.AreEqual(1, reloadedStory.Comments.Count);
+			var result = await _service.GetCommentsForStory(story.Id);
+			Assert.AreEqual(1, result.ResponseOk.Entities.Count);
+		}
+
+		[Test]
+		public async Task TestGetComment()
+		{
+			var story = await _context.Stories
+				.Include(s => s.Comments)
+				.FirstOrDefaultAsync();
+
+			var comment = new Comment { Text = "this is quite a long comment" };
+			story.Comments.Add(comment);
+			_context.Entry(story).State = EntityState.Modified;
+			_context.SaveChanges();
+
+			var result = await _service.GetComment(comment.Id);
+			Assert.True(result.ResponseOk.Text == "this is quite a long comment");
+		}
+
+		[Test]
+		public async Task TestUpdateComment()
+		{
+			var story = await _context.Stories.FirstOrDefaultAsync();
+			
+			var comment = new Comment { Text = "this is a comment", StoryId = story.Id };
+			_context.Comments.Add(comment);
+			_context.SaveChanges();
+
+			var loadedComment = await _context.Comments.FindAsync(comment.Id);
+			loadedComment.Text = "New comment text";
+
+			var result = await _service.UpdateComment(loadedComment);
+			Assert.True(result.ResponseError == null);
+
+			var reloadedComment = await _context.Comments.FindAsync(comment.Id);
+			Assert.AreEqual("New comment text", loadedComment.Text);
+		}
+
+		[Test]
+		public async Task TestDeleteComment()
+		{
+			var story = await _context.Stories
+							.Include(s => s.Comments)
+							.FirstOrDefaultAsync();
+
+			var comment = new Comment { Text = "this is quite a long comment" };
+			story.Comments.Add(comment);
+			_context.Entry(story).State = EntityState.Modified;
+			_context.SaveChanges();
+
+			var commentId = comment.Id;
+
+			await _service.DeleteComment(comment.Id);
+
+			Assert.False(await _context.Comments.AnyAsync(c => c.Id == commentId));
+		}
+
+		[Test]
+		public async Task TestGetFragmentsForStory()
+		{
+			var story = await _context.Stories
+				.Include(s => s.Fragments)
+				.FirstOrDefaultAsync();
+
+			var fragment = new Fragment { Text = "fragment fragment fragment" };
+			story.Fragments.Add(fragment);
+			_context.Entry(story).State = EntityState.Modified;
+			_context.SaveChanges();
+
+			var result = await _service.GetFragmentsForStory(story.Id);
+			Assert.AreEqual(1, result.ResponseOk.Entities.Count);
+		}
+
+		[Test]
+		public async Task TestGetFragment()
+		{
+			var story = await _context.Stories
+				.Include(s => s.Fragments)
+				.FirstOrDefaultAsync();
+
+			var fragment = new Fragment { Text = "fragment fragment fragment" };
+			story.Fragments.Add(fragment);
+			_context.Entry(story).State = EntityState.Modified;
+			_context.SaveChanges();
+
+			var fragmentId = fragment.Id;
+			var result = await _service.GetFragment(fragmentId);
+			Assert.AreEqual(fragmentId, result.ResponseOk.Id);
 		}
 
 		[Test]
@@ -159,8 +277,9 @@ namespace tests
 			var story = await _context.Stories.FirstOrDefaultAsync();
 
 			var author = await _context.ApplicationUsers.FirstOrDefaultAsync();
-			var fragment = new Fragment { Text = "this is a relatively short long fragment", Position = 1, User = author };
-			await _service.AddFragmentToStory(story.Id, fragment);
+			var fragment = new Fragment { Text = "this is a relatively short long fragment", Position = 1, User = author, StoryId = story.Id };
+			_context.Fragments.Add(fragment);
+			_context.SaveChanges();
 
 			var loadedFragment = await _context.Fragments.FindAsync(fragment.Id);
 			loadedFragment.Text = "New fragment text";
@@ -202,6 +321,48 @@ namespace tests
 		}
 
 		[Test]
+		public async Task TestGetTags()
+		{
+			var tag = new Tag { Name = "Tag" };
+			_context.Tags.Add(tag);
+			_context.SaveChanges();
+
+			var loadedTags = await _service.GetTags();
+
+			Assert.AreEqual(1, loadedTags.ResponseOk.Count);
+		}
+
+		[Test]
+		public async Task TestCreateTag()
+		{
+			var tag = new Tag { Name = "Tag" };
+			var result = await _service.CreateTag(tag);
+
+			Assert.True(result.ResponseError == null);
+
+			var tagExists = await _context.Tags.AnyAsync(tag => tag.Id == result.ResponseOk.Id);
+			Assert.True(tagExists);
+		}
+
+		[Test]
+		public async Task TestUpdateTag()
+		{
+			var tag = new Tag { Name = "Tag" };
+			_context.Tags.Add(tag);
+			_context.SaveChanges();
+
+			var loadedTag = await _context.Tags.FindAsync(tag.Id);
+			loadedTag.Name = "New Tag";
+
+			var result = await _service.UpdateTag(loadedTag);
+			Assert.True(result.ResponseError == null);
+
+			var reloadedTag = await _context.Tags.FindAsync(tag.Id);
+			Assert.AreEqual("New Tag", loadedTag.Name);
+		}
+
+
+		[Test]
 		public async Task TestAddTagToStory()
 		{
 			var story = await _context.Stories
@@ -235,7 +396,9 @@ namespace tests
 		[Test]
 		public async Task TestDeleteTag()
 		{
-			var tag = await _context.Tags.FirstOrDefaultAsync();
+			var tag = new Tag { Name = "Tag" };
+			_context.Tags.Add(tag);
+			_context.SaveChanges();
 
 			var tagId = tag.Id;
 
